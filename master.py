cc575d294eb488dcbfa61e4d964316149f84e7b3
Felix Kruger <fkrueger@ebi.ac.uk>
2013-05-22 15:42:43 +0100

    added yaml section to master.py

--- master.py
+++ master.py
@@ -2,51 +2,59 @@
 Function:  master 
 
 Goes through all necessary steps.
-  --------------------
-  Author:
-  Felix Kruger
-  fkrueger@ebi.ac.uk
+    --------------------
+    Author:
+    Felix Kruger
+    fkrueger@ebi.ac.uk
 """  
 
 def master(release, user, pword, host, port): 
-  
+    
 
-  ## Get all human protein coding genes from ensembl and a count of all uniqe domains.
-  import os
-  import pfamDomains
-  import mapPfamDomains
-  import pdbChembl
-  import uniprotChembl
-  import analysis
+    ## Get all human protein coding genes from ensembl and a count of all uniqe domains.
+    import os
+    import pfamDomains
+    import mapPfamDomains
+    import pdbChembl
+    import uniprotChembl
+    import analysis
+    import yaml
+    # Read config file.
+    paramFile = open('mpf.yaml')
+    params = yaml.safe_load(paramFile)
+    user = params['user']
+    pword = params['pword']
+    host = params['host']
+    port = params['port']
+    th = params['threshold']
 
-  #Set the threshold for ligand binding to 50 micromolar
-  th = 50
-  
-  # Get Uniprot identifiers for all human proteins.
-  os.system("R CMD BATCH --vanilla queryBioMaRt.R")
-  # Map Pfam domains and positions to all Uniprot identifiers.
-  pfamDomains.pfamDomains(release, user, pword, host, port)
-  # Map small molecule binding to PFam domains.
-  mapPfamDomains.mapPDs(th, release, user, pword, host, port)
-  # Get all ChEMBL interactions in PDB and binding site residues.
-  pdbDict = pdbChembl.query(release, user, pword, host, port)
-  # Get all ChEMBL interactions in Uniprot and binding site annotation.
-  uniprotDict = uniprotChembl.query(release, user, pword, host, port)
-  # Analyze the data.
-  analysis.analysis(th, release, user, pword, host, port)
-
-if __name__ == '__main__':
-  import sys
-
-  if len(sys.argv) < 5:  # the program name and the two arguments
-
-    sys.exit("Must specify release, user, pword, host, port")
 
     
-  release = sys.argv[1]
-  user = sys.argv[2]
-  pword = sys.argv[3]
-  host = sys.argv[4]
-  port = int(sys.argv[5])
+    # Get Uniprot identifiers for all human proteins.
+    os.system("R CMD BATCH --vanilla queryBioMaRt.R")
+    # Map Pfam domains and positions to all Uniprot identifiers.
+    #pfamDomains.pfamDomains(release, user, pword, host, port)
+    # Map small molecule binding to Pfam domains.
+    mapPfamDomains.mapPDs(th, release, user, pword, host, port)
+    # Get all ChEMBL interactions in PDB and binding site residues.
+    #pdbDict = pdbChembl.query(release, user, pword, host, port)
+    # Get all ChEMBL interactions in Uniprot and binding site annotation.
+    #uniprotDict = uniprotChembl.query(release, user, pword, host, port)
+    # Analyze the data.
+    #analysis.analysis(th, release, user, pword, host, port)
 
-  master(release, user, pword, host, port) 
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 5:  # the program name and the two arguments
+
+        sys.exit("Must specify release, user, pword, host, port")
+
+        
+    release = sys.argv[1]
+    user = sys.argv[2]
+    pword = sys.argv[3]
+    host = sys.argv[4]
+    port = int(sys.argv[5])
+
+    master(release, user, pword, host, port) 
