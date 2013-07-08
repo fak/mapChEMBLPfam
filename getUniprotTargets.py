923e66cd82282bb59fdbc1a8437118aac703e615
Felix Kruger <fkrueger@ebi.ac.uk>
2013-07-08 14:29:16 +0100

    fixed two typos

--- getUniprotTargets.py
+++ getUniprotTargets.py
@@ -10,12 +10,11 @@ def getUniprotTargets(release, user, pword, host, port):
 
   import queryDevice
   
-  rawtargets = queryDevice.queryDevice("SELECT protein_accession, organism\
-FROM target_dictionary WHERE db_source IN('SWISS-PROT', 'TREMBL')", release, user, pword, host, port)
+  rawtargets = queryDevice.queryDevice("""SELECT protein_accession, organism FROM target_dictionary WHERE db_source IN('SWISS-PROT', 'TREMBL')""", release, user, pword, host, port)
 
   targets= {}
   for target in rawtargets:
-    targets[target[0] = target[1]
+    targets[target[0]] = target[1]
 
 
   
