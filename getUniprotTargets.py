ffaca3c70cb98c60d8747eebe8378cbc7908c57a
Felix Kruger <fkrueger@ebi.ac.uk>
2013-07-18 18:17:52 +0100

    added chembl_15 compatibility

--- getUniprotTargets.py
+++ getUniprotTargets.py
@@ -5,18 +5,22 @@ gets all protein accession with db_source swiss-prot or trembl from the Chembl
 target dictionary
 momo.sander@googlemail.com
 """
+import queryDevice
 
 def getUniprotTargets(release, user, pword, host, port):
+    release_number = int(release.split('\_')[1])
+    if release_number >= 15:
+        rawtargets = queryDevice.queryDevice("""SELECT cs.accession
+        FROM component_sequences cs 
+            JOIN target_components tc 
+            ON tc.component_id = cs.component_id  
+        WHERE db_source IN('SWISS-PROT', 'TREMBL')""", release, user, pword, host, port)
+    else:
+        rawtargets = queryDevice.queryDevice("""SELECT protein_accession
+        FROM target_dictionary 
+        WHERE db_source IN('SWISS-PROT', 'TREMBL')""", release, user, pword, host, port)
+    targets= []
+    for target in rawtargets:
+        targets.append(target[0])
+    return targets 
 
-  import queryDevice
-  
-  rawtargets = queryDevice.queryDevice("""SELECT protein_accession, organism FROM target_dictionary WHERE db_source IN('SWISS-PROT', 'TREMBL')""", release, user, pword, host, port)
-
-  targets= {}
-  for target in rawtargets:
-    targets[target[0]] = target[1]
-
-
-  
-
-  return targets
