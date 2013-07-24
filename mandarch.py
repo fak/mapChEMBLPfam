3dd5e2031c4aa7b3d5a5e15b9ab4a389bc8c2ebc
Felix Kruger <fkrueger@ebi.ac.uk>
2013-07-24 21:47:26 +0100

    running multi-domain analysis on eligible ChEMBL targets instead of all ChEMBL targets

--- mandarch.py
+++ mandarch.py
@@ -9,6 +9,7 @@
 ####
 #### import modules.
 ####
+import queryDevice
 import operator
 import numpy as np
 import yaml
@@ -32,6 +33,36 @@ MIN_RATIO = params['min_ratio']
 ####
 #### Define functions.
 ####
+
+def get_el_targets(RELEASE, USER, PWORD, HOST, PORT):
+    data = queryDevice.queryDevice("""
+            SELECT DISTINCT td.protein_accession, dc.dc 
+            FROM target_dictionary td
+            JOIN(
+                      SELECT pd.protein_accession, COUNT(protein_accession) as dc 
+                      FROM pfam_domains pd 
+                      GROUP BY pd.protein_accession
+                     ) as dc
+            ON dc.protein_accession = td.protein_accession
+            JOIN assay2target a2t 
+            ON td.tid = a2t.tid 
+            JOIN assays a 
+            ON a.assay_id = a2t.assay_id 
+            JOIN activities act 
+            ON act.activity_id = a.assay_id
+            WHERE act.standard_type IN('Ki','Kd','IC50','EC50', 'AC50') 
+            AND a2t.multi=0 
+            AND a2t.complex =0 
+            AND a2t.relationship_type = 'D' 
+            AND act.relation = '=' 
+            AND assay_type = 'B' 
+            AND standard_value < 50000
+            AND dc.dc >1;""", RELEASE, USER, PWORD, HOST, PORT)
+    return [x[0] for x in data] 
+
+
+
+
 def readfile(path, key_name, val_name):
     """Read two columns from a tab-separated file into a dictionary.
     Inputs:
@@ -92,19 +123,43 @@ def export_archs(arch_lkp, valid_doms, path):
     Inputs:
     arch_lkp -- dictionary of multi-domain architectures.
     '''
-
     sorted_archs = sorted(arch_lkp.iteritems(), key=operator.itemgetter(1), reverse = True)
     out = open('%s.md' % path ,'w')
     out.write('|architecture|count|mapped|\n')
     out.write('|:-----------|:---------|-----:|\n')
     for arch in sorted_archs:
-        mapped = False
         doms = str(arch[0]).split(', ')
-        if len([x for x in doms if x in valid_doms]) > 0: 
-            mapped = True      
+        mapped = ', '.join([x for x in doms if x in valid_doms]) 
+        if len(mapped) == 0:
+            mapped = False      
         out.write("|%s|%s|%s|\n"%(arch[0], arch[1], mapped))
    
 
+
+def export_network(arch_lkp, valid_doms, path):
+    '''Write out network file.
+    Inputs:
+    arch_lkp -- dictionary of multi-domain architectures.
+    '''
+    lkp = {}
+    for arch in arch_lkp.keys():
+        doms = arch.split(', ')
+        count = arch_lkp[arch]
+        if type(doms) is str:
+            continue
+        for i in range(len(doms)-1):
+            for j in range(i+1, len(doms)):
+                dom_key = ', '.join(sorted([doms[i],doms[j]]))
+                try:
+                   lkp[dom_key] += count
+                except KeyError:
+                   lkp[dom_key] = count
+    out = open('%s.tab' % path ,'w')
+    out.write('dom_1\tdom_2\tcount\n')
+    for link in lkp.keys(): 
+        doms = str(link).split(', ')
+        out.write("%s\t%s\t%s\n"%(doms[0], doms[1], lkp[link]))
+    out.close()
 
 def export_doms(dom_lkp, valid_doms, path):
     '''Write out identified architectures in markdown tables.
@@ -139,13 +194,16 @@ def master(version):
     valid_doms = valid_dom_d.keys()
     ## Load Uniprot targets.
     chembl_targets = getUniprotTargets.getUniprotTargets(RELEASE, USER, PWORD, HOST, PORT)
+    ## Load eligible multi-domain targets.
+    el_targets = get_el_targets(RELEASE, USER, PWORD, HOST, PORT)
     ## Add targets with given architecture.
-    (arch_lkp, dom_lkp) = get_multi_doms(chembl_targets, pfam_d, valid_doms)
+    (arch_lkp, dom_lkp) = get_multi_doms(el_targets, pfam_d, valid_doms)
     ##  Write multi-domain architechtures to markdown tables.
     export_archs(arch_lkp, valid_doms, 'data/multi_dom_archs_%s'% RELEASE)  
     ## Write domains from multi-domain architechtures to markdown tables.
     export_doms(dom_lkp, valid_doms, 'data/multi_dom_doms_%s'% RELEASE)
-
+    ## export network file.
+    export_network(arch_lkp, valid_doms, 'data/multi_dom_network_%s'% RELEASE)
 if __name__ == '__main__':
         import sys
 
