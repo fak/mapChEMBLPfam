b33d9a263cdaa07aeeab31745d583cb5920f36cf
Felix Kruger <fkrueger@ebi.ac.uk>
2013-09-01 23:37:28 +0100

    added a function to export network attributes

--- mandarch.py
+++ mandarch.py
@@ -33,23 +33,22 @@ MIN_RATIO = params['min_ratio']
 ####
 #### Define functions.
 ####
-
 def get_el_targets(RELEASE, USER, PWORD, HOST, PORT):
     data = queryDevice.queryDevice("""
-            SELECT DISTINCT td.protein_accession, dc.dc 
+            SELECT DISTINCT td.protein_accession, dc.dc, COUNT(DISTINCT activity_id) 
             FROM target_dictionary td
             JOIN(
                       SELECT pd.protein_accession, COUNT(protein_accession) as dc 
                       FROM pfam_domains pd 
                       GROUP BY pd.protein_accession
                      ) as dc
-            ON dc.protein_accession = td.protein_accession
+              ON dc.protein_accession = td.protein_accession
             JOIN assay2target a2t 
-            ON td.tid = a2t.tid 
+              ON td.tid = a2t.tid 
             JOIN assays a 
-            ON a.assay_id = a2t.assay_id 
+              ON a.assay_id = a2t.assay_id 
             JOIN activities act 
-            ON act.activity_id = a.assay_id
+              ON act.activity_id = a.assay_id
             WHERE act.standard_type IN('Ki','Kd','IC50','EC50', 'AC50') 
             AND a2t.multi=0 
             AND a2t.complex =0 
@@ -57,7 +56,8 @@ def get_el_targets(RELEASE, USER, PWORD, HOST, PORT):
             AND act.relation = '=' 
             AND assay_type = 'B' 
             AND standard_value < 50000
-            AND dc.dc >1;""", RELEASE, USER, PWORD, HOST, PORT)
+            AND dc.dc >1
+            GROUP BY td.protein_accession ORDER BY COUNT(DISTINCT activity_id)""", RELEASE, USER, PWORD, HOST, PORT)
     return [x[0] for x in data] 
 
 
@@ -86,7 +86,7 @@ def readfile(path, key_name, val_name):
     return  lkp
 
 
-def get_multi_doms(chembl_targets, pfam_d, valid_doms):
+def get_multi_doms(chembl_targets, pfam_d):
     """Find mandatory multi-domain architectures.
     Inputs:
     path -- filepath
@@ -101,7 +101,7 @@ def get_multi_doms(chembl_targets, pfam_d, valid_doms):
         except KeyError:
             print "No entry in Pfam for: %s"% target
             continue
-        inv_doms = [x for x in doms if x not in valid_doms]
+        #inv_doms = [x for x in doms if x not in valid_doms]
         if len(doms) > 1:#len(inv_doms) == len(doms):
             arch = ', '.join(sorted(doms))
             try:
@@ -161,6 +161,27 @@ def export_network(arch_lkp, valid_doms, path):
         out.write("%s\t%s\t%s\n"%(doms[0], doms[1], lkp[link]))
     out.close()
 
+
+def export_attribs(arch_lkp, valid_doms, path):
+    '''Write out network file.
+    Inputs:
+    arch_lkp -- dictionary of multi-domain architectures.
+    '''
+    out = open('%s.tab' % path ,'w')
+    out.write('dom\tvalid\n')
+    lkp = {}
+    for arch in arch_lkp.keys():
+        doms = arch.split(', ')
+        for dom in doms:
+            valid = False
+            if dom in valid_doms:
+                valid = True
+            lkp[dom] = valid
+    for it in lkp.items():
+        out.write("%s\t%s\n"%(it[0], it[1]))
+    out.close()
+
+
 def export_doms(dom_lkp, valid_doms, path):
     '''Write out identified architectures in markdown tables.
     Inputs:
@@ -197,17 +218,19 @@ def master(version):
     ## Load eligible multi-domain targets.
     el_targets = get_el_targets(RELEASE, USER, PWORD, HOST, PORT)
     ## Add targets with given architecture.
-    (arch_lkp, dom_lkp) = get_multi_doms(el_targets, pfam_d, valid_doms)
+    (arch_lkp, dom_lkp) = get_multi_doms(el_targets, pfam_d)
     ##  Write multi-domain architechtures to markdown tables.
     export_archs(arch_lkp, valid_doms, 'data/multi_dom_archs_%s'% RELEASE)  
     ## Write domains from multi-domain architechtures to markdown tables.
     export_doms(dom_lkp, valid_doms, 'data/multi_dom_doms_%s'% RELEASE)
     ## export network file.
     export_network(arch_lkp, valid_doms, 'data/multi_dom_network_%s'% RELEASE)
+    ## export network attribute file.
+    export_attribs(arch_lkp, valid_doms, 'data/multi_dom_attributes_%s'% RELEASE)
 if __name__ == '__main__':
         import sys
 
-        if len(sys.argv) != 2: # the program name and the two arguments
+        if len(sys.argv) != 2: # the program name and one argument
                 sys.exit("""Parameters are read from mpf.yaml but must specify
 		 	    version for data/valid_pfam_v_%(version)s.tab""")
         version = sys.argv[1]
